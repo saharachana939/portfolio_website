b40a46d5e11d6933a96d6a4ae92126b1a3a39d8d
Arachana sah <saharachana939@gmail.com>
2025-09-17 07:54:03 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// const hamburger = document.getElementById("hamburger");
+// const navList = document.querySelector(".navlist");
+// const navLinks = document.querySelectorAll(".navlist li a");
+
+// hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle("active");
+//     navList.classList.toggle("active");
+// });
+
+// navLinks.for
